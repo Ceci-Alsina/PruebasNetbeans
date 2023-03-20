16cceb413a8fba1b5c98caff4f1e9ee578e477f3
Ceci-Alsina <cec.alsina@gmail.com>
2023-03-20 18:20:55 -0300

    commit01

---
+++ EjerciciosFabi1.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciosfabi1;
+
+/**
+ *
+ * @author cecal
+ */
+public class EjerciciosFabi1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 5;
+        int suma = a + b;
+        int resta = a - b;
+        int multi = a * b;
+        int div = a / b;
+        int mo = a % b;
+        System.out.println("suma" +suma);
+        System.out.println("resta" +resta);
+        System.out.println("division" +div);
+        System.out.println("multiplicacion" +multi);
+        System.out.println("El modulo de a y b es" +mo);
+        
+        // TODO code application logic here
+    }
+    
+}
